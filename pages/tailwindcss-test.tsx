d0db624ac1b9b004d69c45559257aaed5c0faee4
Ryosuke Yoshizaki <ryosuke.yoshizaki@kikagaku.co.jp>
2022-01-14 15:06:58 +0900

    Taillwind CSS の設定

---
+++ pages/tailwindcss-test.tsx
@@ -0,0 +1,8 @@
+export default function TailwindcssTestPage(): JSX.Element {
+  return (
+    <div>
+      <h1 className='p-10 text-xl text-red-600'>Tailwind CSS</h1>
+      <h2 className='text-lg text-blue-600'>テストページ</h2>
+    </div>
+  );
+}
